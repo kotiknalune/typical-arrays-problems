65726d265d1aa40616ca6a649ea0f32a6bc17698
Katerina <katerina.raylyan@gmail.com>
2020-02-24 17:19:01 +0300

    implement solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+ if(array && array.length) {
+ 	return Math.min(...array);
+ } else {
+ 	return 0;
+ }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array && array.length) {
+  	return Math.max(...array);
+ } else {
+ 	return 0;
+ }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array && array.length) {
+  	let average = array.reduce((a,b) => a + b, 0) / array.length;
+  	return average;
+ } else {
+ 	return 0;
+ }
 }
